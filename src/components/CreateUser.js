d4c9c7c230eb96bd3f7829a26fefae0f745bf652
Hooseinsalari <hossein.salari002@gmail.com>
2022-09-20 19:28:40 +0430

    pass variables to useMutation for create user

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -5,7 +5,9 @@ import { gql, useMutation } from "@apollo/client";
 
 const CREATE_USER = gql`
   mutation getUser($name: String!, $username: String!, $email: String!) {
-    createUser(input: { name: $name, username: $username, email: $email }) {
+    createUser (
+      input: { name: $name, username: $username, email: $email }
+    ) {
       id
       name
       username
@@ -15,24 +17,19 @@ const CREATE_USER = gql`
 `;
 
 const CreateUser = () => {
-  const [createUser, { loading, data, error, called }] = useMutation(
-    CREATE_USER,
-    {
-      variables: {
-        name: "aa",
-        email: "jj@gna.com",
-        username: "salam",
-      },
-    }
-  );
+    const [createUser, {loading, data, error, called}] = useMutation(CREATE_USER, {
+        variables: {
+            name: "aa",
+            email: "jj@gna.com",
+            username: "salam"
+        }
+    })
 
-  console.log({ loading, data, error, called });
+    console.log({loading, data, error, called});
 
-  return (
-    <div>
-      <button onClick={() => createUser()}>get data</button>
-    </div>
-  );
+  return <div>
+    <button onClick={() => createUser()}>get data</button>
+  </div>;
 };
 
 export default CreateUser;
